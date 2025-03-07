00e8a33fbdd82cb308373dcf71f551b71a0b73a4
Minh Huy Do <dominhhuy1703@gmail.com>
2025-03-07 14:06:29 +0100

    [HUY] Change nodelink, bar chart

--- mdn_webcomponent/src/components/my-component/nodelink-chart.js
+++ mdn_webcomponent/src/components/my-component/nodelink-chart.js
@@ -1,80 +1,98 @@
 class NodeLinkChart extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-  }
-
-  static get observedAttributes() {
-    return ['data'];
-  }
-
-  connectedCallback() {
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+    }
+  
+    static get observedAttributes() {
+      return ['data'];
+    }
+  
+    connectedCallback() {
       this.render();
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
+    }
+  
+    attributeChangedCallback(name, oldValue, newValue) {
       this.drawChart();
-  }
-
-  render() {
+    }
+  
+    render() {
       this.shadowRoot.innerHTML = `
-      <svg width="400" height="400"></svg>`;
-  }
-
-  drawChart() {
+        <svg width="400" height="400"></svg>
+      `;
+    }
+  
+    drawChart() {
       const data = JSON.parse(this.getAttribute('data'));
       const svgElement = this.shadowRoot.querySelector('svg');
       const width = 400, height = 400;
-
+  
       d3.select(svgElement).selectAll("g").remove();
-
+  
       const svg = d3.select(svgElement)
-          .attr("width", width)
-          .attr("height", height)
-          .append("g")
-      .attr("transform",
-          `translate(0, 0)`);
-
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", `translate(0, 0)`);
+  
       // Initialize the links
       const link = svg
-      .selectAll("line")
-      .data(data.links)
-      .join("line")
-          .style("stroke", "#aaa")
-
-
+        .selectAll("line")
+        .data(data.links)
+        .join("line")
+        .style("stroke", "#aaa");
+  
       // Initialize the nodes
       const node = svg
-      .selectAll("circle")
-      .data(data.nodes)
-      .join("circle")
-          .attr("r", 20)
-          .style("fill", "#69b3a2");
-
-
-      // Let's list the force we wanna apply on the network
-      const simulation = d3.forceSimulation(data.nodes)                 // Force algorithm is applied to data.nodes
-          .force("link", d3.forceLink()                               // This force provides links between nodes
-              .id(function(d) { return d.id; })                     // This provide  the id of a node
-              .links(data.links)                                    // and this the list of links
-          )
-          .force("charge", d3.forceManyBody().strength(-1000))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-          .force("center", d3.forceCenter(width / 1.2, height / 2))     // This force attracts nodes to the center of the svg area
-          .on("end", ticked);
-
-      // This function is run at each iteration of the force algorithm, updating the nodes position.
+        .selectAll("circle")
+        .data(data.nodes)
+        .join("circle")
+        .attr("r", 20)
+        .style("fill", "#69b3a2");
+  
+      // Force simulation
+      const simulation = d3.forceSimulation(data.nodes)
+        .force("link", d3.forceLink().id(d => d.id).links(data.links))
+        .force("charge", d3.forceManyBody().strength(-1000))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .on("tick", ticked);
+  
       function ticked() {
-      link
-          .attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
-
-      node
-          .attr("cx", function (d) { return d.x+6; })
-          .attr("cy", function(d) { return d.y-6; });
-      };
+        link
+          .attr("x1", d => d.source.x)
+          .attr("y1", d => d.source.y)
+          .attr("x2", d => d.target.x)
+          .attr("y2", d => d.target.y);
+  
+        node
+          .attr("cx", d => d.x)
+          .attr("cy", d => d.y);
+      }
+  
+      // Adding drag functionality to nodes
+      node.call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+  
+      function dragstarted(event) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        event.subject.fx = event.subject.x;
+        event.subject.fy = event.subject.y;
+      }
+  
+      function dragged(event) {
+        event.subject.fx = event.x;
+        event.subject.fy = event.y;
+      }
+  
+      function dragended(event) {
+        if (!event.active) simulation.alphaTarget(0);
+        event.subject.fx = null;
+        event.subject.fy = null;
+      }
+    }
   }
-}
-
-customElements.define('nodelink-chart', NodeLinkChart);
+  
+  customElements.define('nodelink-chart', NodeLinkChart);
+  
