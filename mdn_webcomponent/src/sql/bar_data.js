849b10b11352507f583957940bfddec090f733d3
Minh Huy Do <dominhhuy1703@gmail.com>
2025-04-25 10:14:07 +0100

    [HUY] Encapsulation the functions to fetch SPARQL data

---
+++ mdn_webcomponent/src/sql/bar_data.js
@@ -0,0 +1,61 @@
+// Function make query for BarData
+async function fetchBarData() {
+    const query = `
+        PREFIX dbpprop: <http://dbpedia.org/property/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX yago: <http://dbpedia.org/class/yago/>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+        SELECT DISTINCT ?name ?population (SAMPLE(?lang) AS ?randomLang)
+        WHERE {
+        ?country a dbo:Country ;
+                rdfs:label ?enName ;
+                dbo:language [ rdfs:label ?lang ] .
+        
+        OPTIONAL { ?country dbpprop:populationEstimate ?popEstimate. }
+        OPTIONAL { ?country dbpprop:populationCensus ?popCensus. }
+        BIND(coalesce(?popEstimate, ?popCensus) AS ?populationRaw)
+        
+        FILTER (
+            EXISTS { ?country (dbpprop:iso3166code|dbpprop:iso31661Alpha|dbpprop:countryCode) ?code }
+            || EXISTS { ?country a yago:MemberStatesOfTheUnitedNations }
+        )
+        
+        FILTER(langMatches(lang(?enName), "en"))
+        
+        BIND(
+            IF(datatype(?populationRaw) = xsd:integer,
+            ?populationRaw,
+            xsd:integer(?populationRaw)
+            ) AS ?population
+        )
+        FILTER(BOUND(?population))
+        
+        BIND(str(?enName) AS ?name)
+        }
+        GROUP BY ?name ?population
+        LIMIT 10
+    `;
+    return await fetchSPARQLData(query);
+}
+
+// Function to get SPARQLData
+async function fetchSPARQLData(query) {
+    const endpointUrl = "https://dbpedia.org/sparql";
+    const url = `${endpointUrl}?query=${encodeURIComponent(query)}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Accept': 'application/sparql-results+json' }
+        });
+
+        const jsonData = await response.json();
+
+        return jsonData.results ? jsonData.results.bindings : [];
+    } catch (error) {
+        console.error("Error fetching SPARQL data:", error);
+        return [];
+    }
+}
